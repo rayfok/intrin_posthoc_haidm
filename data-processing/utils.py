9e14f32ebbba1c871d0fd327a7737f798c132517
Raymond Fok <rayfok@umich.edu>
2022-04-20 14:32:06 -0700

    add c_charge_desc to feature list for compas

--- data-processing/utils.py
+++ data-processing/utils.py
@@ -14,13 +14,17 @@ def load_compas_data(use_race=False):
         "priors_count",
         "c_charge_degree",
         "two_year_recid",
+        "c_charge_desc",
     ]
+
     one_hot_cols = ["sex", "c_charge_degree"]
     if use_race:
         keep_cols.append("race")
         one_hot_cols.append("race")
+
     df = df[keep_cols]
     one_hot_df = df.copy(deep=True)
+
     # for x in one_hot_cols:
     #     one_hot = pd.get_dummies(df[x], prefix=x)
     #     one_hot_df = one_hot_df.drop(x, axis=1)
@@ -28,5 +32,5 @@ def load_compas_data(use_race=False):
 
     one_hot_df["is_male"] = one_hot_df["sex"].map({"Female": 0, "Male": 1})
     one_hot_df["is_felony"] = one_hot_df["c_charge_degree"].map({"M": 0, "F": 1})
-    one_hot_df.drop(columns=["sex", "c_charge_degree"], inplace=True)
+    one_hot_df.drop(columns=["sex", "c_charge_degree", "c_charge_desc"], inplace=True)
     return df, one_hot_df

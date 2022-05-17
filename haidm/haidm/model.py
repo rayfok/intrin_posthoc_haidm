a6f47efcc65e077a25e5d277052912d37e4abb5b
Raymond Fok <rayfok@cs.washington.edu>
2022-05-17 00:40:43 +0000

    add missing dataclass typing to model schema

--- haidm/haidm/model.py
+++ haidm/haidm/model.py
@@ -8,20 +8,20 @@ db = SQLAlchemy()
 
 @dataclass
 class Response(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    worker_id = db.Column(db.String(80), nullable=True)
-    hit_id = db.Column(db.String(80), nullable=True)
-    assignment_id = db.Column(db.String(80), nullable=True)
-    task = db.Column(db.String(80), nullable=False)
-    condition = db.Column(db.String(80), nullable=True)
-    question_id = db.Column(db.String(80), nullable=False)
-    initial_human_decision = db.Column(db.String(80), nullable=True)
-    final_human_decision = db.Column(db.String(80), nullable=False)
-    ai_decision = db.Column(db.String(80), nullable=True)
-    initial_decision_time = db.Column(db.Integer, nullable=False)
-    final_decision_time = db.Column(db.Integer, nullable=False)
-    ground_truth = db.Column(db.String(80), nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id: int = db.Column(db.Integer, primary_key=True)
+    worker_id: str = db.Column(db.String(80), nullable=True)
+    hit_id: str = db.Column(db.String(80), nullable=True)
+    assignment_id: str = db.Column(db.String(80), nullable=True)
+    task: str = db.Column(db.String(80), nullable=False)
+    condition: str = db.Column(db.String(80), nullable=True)
+    question_id: str = db.Column(db.String(80), nullable=False)
+    initial_human_decision: str = db.Column(db.String(80), nullable=True)
+    final_human_decision: str = db.Column(db.String(80), nullable=False)
+    ai_decision: str = db.Column(db.String(80), nullable=True)
+    initial_decision_time: int = db.Column(db.Integer, nullable=False)
+    final_decision_time: int = db.Column(db.Integer, nullable=False)
+    ground_truth: str = db.Column(db.String(80), nullable=False)
+    created: datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<Response {self.worker_id} - {self.task} - {self.condition}>"
@@ -51,13 +51,13 @@ class ExitSurveyResponse(db.Model):
 
 @dataclass
 class Session(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    workerId = db.Column(db.String(80), nullable=True)
-    hitId = db.Column(db.String(80), nullable=True)
-    assignmentId = db.Column(db.String(80), nullable=True)
-    task = db.Column(db.String(80), nullable=True)
-    condition = db.Column(db.String(80), nullable=True)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id: int = db.Column(db.Integer, primary_key=True)
+    workerId: str = db.Column(db.String(80), nullable=True)
+    hitId: str = db.Column(db.String(80), nullable=True)
+    assignmentId: str = db.Column(db.String(80), nullable=True)
+    task: str = db.Column(db.String(80), nullable=True)
+    condition: str = db.Column(db.String(80), nullable=True)
+    created: datetime = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<Worker {self.workerId} - {self.hitId} - {self.assignmentId}>"
